3c3cefe5fd19b2777fc5ccf1d1b89428e7c0c0a7
valeria <kozhevnikovavaleria91@gmail.com>
2021-04-23 11:47:50 +0300

    first commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    $.gulp.task('watch',function(){
+        $.gulp.watch('assets/**/*.html',$.gulp.series('html'));
+        $.gulp.watch('assets/scss/**/*.scss',$.gulp.series('sass'));
+        $.gulp.watch('assets/js/*.js',$.gulp.series('scripts'));
+        $.gulp.watch('assets/js/libs/**/*.js',$.gulp.series('scripts:lib'));
+        // $.gulp.watch('assets/img/**/*',$.gulp.series('img'));
+        $.gulp.watch('assets/fonts/**/*',$.gulp.series('fonts'));
+    });
+}
